f9969f780b6c58dd5f83de9393f1aca85aba6f81
xl69 <xl69@illinois.edu>
2022-02-28 22:04:52 -0600

    Add UF_pc

---
+++ Templates/Union_Find/Union_Find_With_Path_Compression.java
@@ -0,0 +1,16 @@
+class DSU {
+	int[] parent;
+	public DSU(int n) {
+		parent = new int[n];
+		for (int i = 0; i < n; i++) parent[i] = i;
+	}
+	public int find(int x) {
+		if (parent[x] != x) {
+			parent[x] = find(parent[x]);
+		}
+		return parent[x];
+	}
+	public void union(int x, int y) {
+		parent[find(x)] = find(y);
+	}
+}
